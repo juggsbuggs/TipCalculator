a8dcf6c87f2da9c1c825be8ad939095e80516cc2
juggsbuggs <99005864+juggsbuggs@users.noreply.github.com>
2022-04-15 23:19:32 +0530

    Update tip.py

--- tip.py
+++ tip.py
@@ -1,1 +1,11 @@
-print("Welcome")
+print("Welcome to the tip calculator!")
+total = input("\nWhat was the total bill?")
+tip = input("\nHow much would you tip? 10%, 12%, or 15%?")
+people = input("\nHow many people to split among?")
+
+cal_tip = int(total) * (int(tip) / 100)
+cal_total = int(total) + cal_tip
+cal_split = cal_total / int(people)
+
+message = f"Each person should pay: ${cal_split:.2f}"
+print(message)
